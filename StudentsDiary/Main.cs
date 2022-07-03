e420b359f4c93a68e001872c06b94eee8e63886b
RokerDev <rokerdev91@gmail.com>
2022-07-03 17:05:46 +0200

    prepare serialize and deserialize methods to save data

--- StudentsDiary/Main.cs
+++ StudentsDiary/Main.cs
@@ -3,18 +3,50 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Serialization;
 
 namespace StudentsDiary
 {
     public partial class Main : Form
     {
+        private string _filePath = Path.Combine(Environment.CurrentDirectory, "students.txt");
+
         public Main()
         {
             InitializeComponent();
+        }
+
+        public void SerializeToFile(List<Student> students)
+        {
+            var serializer = new XmlSerializer(typeof(List<Student>));
+            using (var streamWriter = new StreamWriter(_filePath))
+            {
+                serializer.Serialize(streamWriter, students);
+                streamWriter.Close();
+            }
+        }
+        
+        public List<Student> DeserializeFile()
+        {
+            if (!File.Exists(_filePath))
+            {
+                return new List<Student>();
+            }
+            var serializer = new XmlSerializer(typeof(List<Student>));
+
+            using (var streamReader = new StreamReader(_filePath))
+            {
+                var students = (List<Student>)serializer.Deserialize(streamReader);
+                streamReader.Close();
+
+                return students;
+            }
+
         }
 
 
